b7e72cb193894cd78f4da203deb84cbc9e1545e3
Michael Serero <58833631+mcxs-cg@users.noreply.github.com>
2021-08-02 20:50:08 -0700

    Update clean_visit_refactored.sql

--- clean_visit_refactored.sql
+++ clean_visit_refactored.sql
@@ -9,35 +9,35 @@ drop table #VISITS_BASE_TABLE
 create table MSS_U_INFODEL_S.VISITS_BASE_TABLE
 with (heap, distribution = hash(VisitID)) as
 select	
-        a.ID									as VisitID
-        ,a.CREATEDDATE							as CreatedDate
-        ,a.CREATOR_ROLE__C						as CreatorRole
-        ,a.CREATEDBYID							as CreatedByID			-- Creator of visit record
+        a.ID							as VisitID
+        ,a.CREATEDDATE						as CreatedDate
+        ,a.CREATOR_ROLE__C					as CreatorRole
+        ,a.CREATEDBYID						as CreatedByID			-- Creator of visit record
         ,a.START_DATE_TIME__C					as StartDateTime
-        ,a.END_DATE_TIME__C						as EndDateTime	
-        ,a.DURATION__C							as Duration				-- Visit duration in minutes 
+        ,a.END_DATE_TIME__C					as EndDateTime	
+        ,a.DURATION__C						as Duration			-- Visit duration in minutes 
         ,a.VISIT_ATTENDEES__C					as VisitAttendees		-- Number of visit attendees
-        ,a.VISIT_TYPE__C						as VisitType
-        ,a.NOTES__C								as Notes
-        ,a.STATUS__C							as Status
-        ,a.CAPTURED_FEEDBACK_VIA__C				as CapturedFeedbackVia	-- How visit feedback was captured: Voice, Text, etc.
+        ,a.VISIT_TYPE__C					as VisitType
+        ,a.NOTES__C						as Notes
+        ,a.STATUS__C						as Status
+        ,a.CAPTURED_FEEDBACK_VIA__C				as CapturedFeedbackVia		-- How visit feedback was captured: Voice, Text, etc.
         ,a.MARKETING_EVENT__C					as MarketingEventID
-        ,a.MARKETING_EVENT_ATTENDEES__C			as MarketingEventAttendees
-        ,a.NAME									as Name					-- Visit description
+        ,a.MARKETING_EVENT_ATTENDEES__C				as MarketingEventAttendees
+        ,a.NAME							as Name				-- Visit description
         ,a.ALL_OWNER_INITIALS__C				as AllOwnerInitials		-- List of owners for the visit
         ,a.OWNER_INITIALS__C					as OwnerInitials		-- Primary visit owner initials
-        ,a.OWNER_TITLE__C						as OwnerTitle			-- Primary visit owner title
-        ,a.FEEDBACK__C							as Feedback
+        ,a.OWNER_TITLE__C					as OwnerTitle			-- Primary visit owner title
+        ,a.FEEDBACK__C						as Feedback
         ,a.PRIMARY_CONTACT__C					as PrimaryContact
-        ,a.FOCUS__C	 							as Focus				-- Focus code for a visit part of a Marketing 'Focus' campaign
+        ,a.FOCUS__C	 					as Focus			-- Focus code for a visit part of a Marketing 'Focus' campaign
         ,a.NUM_OF_ATTENDEES__C					as NumOfAttendees 
-		,a.MEETING_STATUS__C					as MeetingStatus 
-		,a.TIMETRADE_MEETING__C					as TimeTradeMeeting 
-		,a.TIMETRADE_MEETING_INVITATION__C		as TimeTradeMeetingInvitation
-		,a.VIRTUAL__C							as Virtual
-		,b.STATUS__C 							as AttendeeStatus	
-        ,b.NOTES__C 							as AttendeeNotes
-        ,b.CONTACT__C							as AttendeeContactID
+	,a.MEETING_STATUS__C					as MeetingStatus 
+	,a.TIMETRADE_MEETING__C					as TimeTradeMeeting 
+	,a.TIMETRADE_MEETING_INVITATION__C			as TimeTradeMeetingInvitation
+	,a.VIRTUAL__C						as Virtual
+	,b.STATUS__C 						as AttendeeStatus	
+        ,b.NOTES__C 						as AttendeeNotes
+        ,b.CONTACT__C						as AttendeeContactID
 	
 from
 	MSS_S_SFDC_S.vw_sfdc_visit__c_current a
